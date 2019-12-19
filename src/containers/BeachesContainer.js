e32ec3808ae362027e70c4c6570830652f3834fe
James Sinker <j@Jamess-MacBook-Pro.local>
2019-12-18 16:04:22 -0800

    creates ability to add to beaches table with board options included

--- src/containers/BeachesContainer.js
+++ src/containers/BeachesContainer.js
@@ -1,11 +1,57 @@
 import React, { Component } from 'react'
+import BeachesModel from '../models/Beaches'
 
 class BeachesContainer extends Component {
+  state = {
+    createBeachName:'newone',
+    createBeachDescription:'such a great beach',
+    boardTypeIds:[]
+  }
+  
+  boardChoose = (e)=> {
+    e.preventDefault();
+    let boardTypeId = Number(e.target.value)
+    this.setState({boardTypeIds: [...this.state.boardTypeIds, boardTypeId]}) 
+    console.log(e.target.value)
+  }
+  
+  nameChange = (e)=> {
+    this.setState({createBeachName: (e.target.value)})
+  }
+  descChange = (e)=> {
+    this.setState({createBeachDescription: (e.target.value)})
+  }
+
+  createBeach = (e)=> {
+    e.preventDefault()
+    let newBeach= {
+      name: this.state.createBeachName,
+      description: this.state.createBeachDescription,
+      boardTypeIds: this.state.boardTypeIds
+    }  
+    BeachesModel.create(newBeach).then((res)=> {
+      this.setState({createBeachName:'', createBeachDescription:'', boardTypeIds:[]})
+    }) 
+  }
+
   render (){
     return(
-      this.props.data.map((beach, id) => {
-      return <div key={id}>{beach.name}</div>
-      })
+      <div>
+        <form type='input'>
+          <input onChange={this.nameChange} id='beachName' placeholder='beach name'></input>
+          <input onChange={this.descChange} id='description' placeholder='description'></input>
+          <p>Choose below which boards you should ride here!</p> 
+          <div className="topicsContainer">
+            <button className={ this.state.boardTypeIds.includes(1) ? 'boardType active' : 'boardType' } onClick={this.boardChoose} value={1}>Shortboard</button>
+            <button className={ this.state.boardTypeIds.includes(2) ? 'boardType active' : 'boardType' } onClick={this.boardChoose} value={2}>Longboard</button>
+            <button className={ this.state.boardTypeIds.includes(3) ? 'boardType active' : 'boardType' } onClick={this.boardChoose} value={3}>fish</button>
+            <button className={ this.state.boardTypeIds.includes(4) ? 'boardType active' : 'boardType' } onClick={this.boardChoose} value={4}>gun</button>
+            <button className={ this.state.boardTypeIds.includes(5) ? 'boardType active' : 'boardType' } onClick={this.boardChoose} value={5}>funboard</button>
+            <button className='createBoardButton' onClick={this.createBeach}>CREATE NEW BEACH</button>
+          </div>
+
+        </form>
+      </div>
     )
   }
 }
--- src/models/Beaches.js
+++ src/models/Beaches.js
@@ -6,7 +6,18 @@ class BeachesModel {
       .then(response => response.json())
       .catch(err => console.log("couldn't get all beaches\n", err));
   };
-
+    
+  static create = (beach) => {
+    return fetch(endpoint, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(beach)
+    })
+      .then(response =>response.json())
+      .catch(error => console.log('could not make new beach \n', error));
+  }
 }
 
 export default BeachesModel
